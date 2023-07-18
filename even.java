5f2248736d656e2491b451347cb601a89536f5ce
Apreity <139572006+Apreity@users.noreply.github.com>
2023-07-18 13:14:23 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class even
+{
+public static void main(String args[])
+{
+int count=0;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0){
+System.out.print("number is even");
+}else{
+System.out.print("number is odd");
+}
+}
+}
